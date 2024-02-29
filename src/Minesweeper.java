b3d9f11eda0fc226242850fda467ded858623f57
unknown <johnogunsola112@gmail.com>
2024-02-29 10:12:04 +0000

    first commit

---
+++ src/Minesweeper.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Minesweeper {
+    int x = 5;
+    public static void main(String[] args) {
+        Minesweeper line1 = new Minesweeper();
+        System.out.println(Minesweeper.x);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+
+
+
+    }
+}
