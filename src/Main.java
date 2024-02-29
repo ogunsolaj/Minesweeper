50532899b60bec03fabb544be37b42bf6ac15694
unknown <johnogunsola112@gmail.com>
2024-02-29 12:33:04 +0000

    before creating the board

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    Minesweeper1 p1 = new Minesweeper1();
+    p1.positionx = 1;
+    p1.positiony = 1;
+    System.out.println(p1.bomb);
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Minesweeper1.java
@@ -0,0 +1,11 @@
+public class Minesweeper1 {
+    int positionx;
+    int positiony;
+    boolean bomb;
+
+    Minesweeper1(){
+        bomb = false;
+    }
+
+}
+
